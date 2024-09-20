a15f753753efb7fa64cf1d3fb79338d60e2b5616
Siva Nanditha Kanneti <155439558+SivaNanditha@users.noreply.github.com>
2024-09-20 12:34:45 +0530

    Add files via upload
    XSD TO JAVA POJO

---
+++ admi004/src/main/java/com/Fednow/admi004/Admi004Application.java
@@ -0,0 +1,13 @@
+package com.Fednow.admi004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Admi004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Admi004Application.class, args);
+	}
+
+}
---
+++ admi004/src/test/java/com/Fednow/admi004/Admi004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fednow.admi004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Admi004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
